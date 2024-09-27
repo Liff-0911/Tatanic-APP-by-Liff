a7a14d6550dc3e43616103dd032f20ac777b3574
Liff-0911 <liff_0911@163.com>
2024-09-27 15:57:33 +0800

    Add files via upload

--- assignment-st-starter.py
+++ assignment-st-starter.py
@@ -9,6 +9,7 @@ st.title('Titanic APP by Liff')
 
 # read csv and show the dataframe
 df = pd.read_csv('train.csv')
+df
 
 # create a figure with three subplots, size should be (15, 5)
 fig, ax = plt.subplots(1,3,figsize = (15,5))
@@ -19,4 +20,5 @@ df[df.Pclass == 3].Fare.plot.box(ax = ax[2]).set_xlabel('Pclass=3')
 (df.groupby(by=[df.Pclass,df.Survived]).Survived.value_counts()/df.Pclass.value_counts()).plot.bar(figsize = (5,3))
 # you need to set the x labels and y labels
 # a sample diagram is shown below
+st.pyplot(fig)
 
